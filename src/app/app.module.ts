b67a587e3a3faf22d4962e8b58d327445dc42418
MetropoliaJL <joonas.lassila95@gmail.com>
2017-01-30 13:34:18 +0200

    -Create api-helper service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ApiHelperService } from './services/api-helper.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ApiHelperService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
