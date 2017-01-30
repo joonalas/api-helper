324b32571cf4ea0a0886afe68f92834b44f68ca6
MetropoliaJL <joonas.lassila95@gmail.com>
2017-01-30 13:42:22 +0200

    -App component for testing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+
+import { APIHelperService } from './services/api-helper.service';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  title = 'API-helper service';
+  private token: string;
+
+  constructor(private apiHelperService: APIHelperService) {}
+
+  ngOnInit() {
+    this.apiHelperService.login('joonuska', 'RagnaxJIN7113').subscribe(
+      (resp) => console.log(resp)
+    );
+  }
+
+  modUser() {
+    this.apiHelperService.modUser('joonuska', null, null, this.token).subscribe(
+      (resp) => console.log(resp)
+    );
+  }
+
 }
