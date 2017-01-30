6c89d8506157bc64c40a8892190ff0f0a3a949c1
joonas <joonas.lassila95@gmail.com>
2017-01-30 05:06:55 +0200

    -Add user methods.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'API-helper service';
+  private token: string;
 
   constructor(private apiHelperService: APIHelperService) {}
 
   ngOnInit() {
     this.apiHelperService.login('joonalas', 'RagnaxJIN7113').subscribe(
+      (res) => {
+        console.log(res);
+        this.token = res.token;
+      }
+    );
+  }
+
+  modUser() {
+    this.apiHelperService.modUser('joonuska', null, null, this.token).subscribe(
       (res) => console.log(res)
     );
   }
--- src/app/services/api-helper.service.ts
+++ src/app/services/api-helper.service.ts
@@ -15,7 +15,8 @@ export class APIHelperService {
 
   constructor(private http: Http) { }
 
-// Authentication. Returns observable consisting of JSON objects.
+
+  // Authentication. Returns observable consisting of JSON objects.
   login(username: string, password: string) {
     const url = this.baseURL + '/login';
     const headers = new Headers({'Content-Type': 'application/json'});
@@ -30,6 +31,66 @@ export class APIHelperService {
     return this.http.post(url, data, options).map(
       /* Response is in JSON string form. Transform it to javascript object with
       json() function */
+      resp => resp.json()
+    );
+  }
+
+
+  // User. TODO: optional field full_name.
+  createUser(username: string, password: string, email: string) {
+    const url = this.baseURL + '/users';
+    const headers = new Headers({'Content-Type': 'application/json'});
+    const options = new RequestOptions({headers: headers});
+    const data = `
+      {
+        "username": "${username}",
+        "password": "${password}",
+        "email": "${email}"
+      }
+    `;
+
+    return this.http.post(url, data, options).map(
+      resp => resp.json()
+    );
+  }
+
+  deleteUser(id: Number, token: string) {
+    const url = this.baseURL + '/users';
+    const headers = new Headers({'Content-Type': 'application/json'});
+    headers.append('x-access-token', token);
+    const options = new RequestOptions({headers: headers});
+
+    return this.http.delete(url, options).map(
+      resp => resp.json()
+    );
+  }
+
+  modUser(username: string, password: string, email: string, token: string) {
+    console.log(token);
+    const jsonObject = {};
+    if (username) {
+      jsonObject['username'] = username;
+    }
+    if (password) {
+      jsonObject['password'] = password;
+    }
+    if (email) {
+      jsonObject['email'] = email;
+    }
+
+    const url = this.baseURL + '/users';
+    const headers = new Headers({'Content-Type': 'application/json'});
+    headers.append('x-access-token', token);
+    //headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Headers',
+    'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token, origin, cache-control');
+    headers.append('Access-Control-Allow-Methods', '*');
+    //headers.append('Cache-Control', 'no-cache');
+    const options = new RequestOptions({headers: headers});
+    const data = JSON.stringify(jsonObject);
+    console.log(data, headers);
+
+    return this.http.post(url, data, options).map(
       resp => resp.json()
     );
   }
